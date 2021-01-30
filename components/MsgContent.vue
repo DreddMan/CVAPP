7b84ced18597963f2edbf17b035b9ee63aec0f2a
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-01-30 13:11:53 +0000

    fix test cases

--- components/MsgContent.vue
+++ components/MsgContent.vue
@@ -8,4 +8,4 @@ const md = new MarkdownIt({
   linkify: true,
   highlight(code, lang) {
     const language = hljs.getLanguage(lang) ? lang : 'plaintext'
-    return `<pre class="hljs-code-container my-3"><div class="hljs-code-header d-flex align-center justify-space-between bg-grey-dark
+    return `<pre class="hljs-code-container my-3"><div class="hljs-code-header d-flex align-center justify-space-between bg-grey-darken-3 pa-1"><span class="pl-2 text-capti
