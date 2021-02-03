dae35746e3292ad372a898eb6851a1a5db47fffe
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-02-03 13:14:48 +0000

    More Testing

--- components/MsgContent.vue
+++ components/MsgContent.vue
@@ -8,4 +8,4 @@ const md = new MarkdownIt({
   linkify: true,
   highlight(code, lang) {
     const language = hljs.getLanguage(lang) ? lang : 'plaintext'
-    return `<pre class="hljs-code-container my-3"><div class="hljs-code-header d-flex align-center justify-space-between bg-grey-darken-3 pa-1"><span class="pl-2 text-caption">${language}</span><button class="hljs-copy-button" data-copied="false">Copy</button></div><code class="hljs langu
+    return `<pre class="hljs-code-container my-3"><div class="hljs-code-header d-flex align-center justify-space-between bg-grey-darken-3 pa-1"><span class="pl-2 text-caption">${language}</span><button class="hljs-copy-button" data-copied="false">Copy</button></div><code class="hljs language-${language}">${hljs.highlight(cod
