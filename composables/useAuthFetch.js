ae072ab085153b414eadfcbda699fb8fb790d3bf
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-06-19 21:52:19 +0000

    Add configuration files

--- composables/useAuthFetch.js
+++ composables/useAuthFetch.js
@@ -2,4 +2,5 @@ export const useAuthFetch = async (url, options = {}) => {
     const { $auth } = useNuxtApp()
 
     const res = await useFetch(url, options)
-    if (res.error.value && res.err
+    if (res.error.value && res.error.value.status === 401) {
+        await $auth.
