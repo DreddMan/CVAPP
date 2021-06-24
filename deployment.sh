aa5f2ea68b9e3f044ea421b988c67b604469ee4a
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-06-24 06:10:41 +0000

    Increment Version

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-read -p "Please enter a domain name or external IP address [default: localhost]: " APP_DO
+read -p "Please enter a domain name or external IP address [default: localhost]: " APP_DOMAIN
+
+if [ -z "$APP_DOMAIN" ]; then
+  
