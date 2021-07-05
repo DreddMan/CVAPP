9ab191ef419cb13242ef8e2b438e42971df70c8b
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-07-05 16:36:56 +0000

    Test refactor

--- deployment.sh
+++ deployment.sh
@@ -39,4 +39,4 @@ else
 
     sudo mkdir -m 0755 -p /etc/apt/keyrings
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | 
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.
