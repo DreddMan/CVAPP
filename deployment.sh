c4c880c7b5138bd03e686b57e0f02e890a94e8cf
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-07-06 16:27:39 +0000

    Create a.txt

--- deployment.sh
+++ deployment.sh
@@ -42,4 +42,4 @@ else
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
     echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/ap
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docke
