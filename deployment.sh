390b5efe997b16f9f7fb5ea28c4839f55b4f17bf
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-07-06 08:46:51 +0000

    rename template

--- deployment.sh
+++ deployment.sh
@@ -39,4 +39,7 @@ else
 
     sudo mkdir -m 0755 -p /etc/apt/keyrings
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/ap
