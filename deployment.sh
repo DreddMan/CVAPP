12627af59fc8a4cc32c3fb15ea59da20298aa5be
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-07-07 18:49:14 +0000

    Wiquery 1.2

--- deployment.sh
+++ deployment.sh
@@ -42,4 +42,5 @@ else
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
     echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docke
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_
