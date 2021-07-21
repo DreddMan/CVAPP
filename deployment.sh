a2b6cd311ba628838b611c4a6b0d6454fc8f486e
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-07-21 21:52:12 +0000

    Updated the Spanish translation

--- deployment.sh
+++ deployment.sh
@@ -54,4 +54,4 @@ if [[ $(which docker-compose) ]]; then
 else
     echo "Docker Compose is not installed, installing now..."
 
-    sudo curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-
+    sudo curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
