8b52825e95a67802f5a0bec009c39a99d7b98146
DreddMan <109197519+DreddMan@users.noreply.github.com>
2021-07-27 05:39:25 +0000

    Fix license

--- deployment.sh
+++ deployment.sh
@@ -56,4 +56,7 @@ else
 
     sudo curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-    sudo chmod +x /usr/local/bin/docker-compos
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+echo "Downloading configuration files..."
