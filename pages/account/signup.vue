81645f972b2e2fde8377c1388bbdc047ee26bc21
DreddMan <109197519+DreddMan@users.noreply.github.com>
2022-03-07 14:45:42 +0000

    Ignore

--- pages/account/signup.vue
+++ pages/account/signup.vue
@@ -103,4 +103,5 @@ const handleFieldUpdate = (field) => {
             <v-card-text>
               <v-form ref="signUpForm" class="mt-5">
                 <v-text-field
-                
+                    v-model="formData.username"
+            
