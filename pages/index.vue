6855416628a8647e76d044c7bb6ddbf216df09a3
DreddMan <109197519+DreddMan@users.noreply.github.com>
2022-06-13 05:43:32 +0000

    alpha: fix fpu.h usage in userspace

--- pages/index.vue
+++ pages/index.vue
@@ -58,3 +58,52 @@ const fetchReply = async (message, parentMessageId) => {
       method: 'POST',
       headers: {
         'accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        model: currentModel.value,
+        openaiApiKey: openaiApiKey.value,
+        message: message,
+        parentMessageId: parentMessageId,
+        conversationId: currentConversation.value.id
+      }),
+      onopen(response) {
+        if (response.ok && response.headers.get('content-type') === EventStreamContentType) {
+          return;
+        }
+        throw new Error(`Failed to send message. HTTP ${response.status} - ${response.statusText}`);
+      },
+      onclose() {
+        if (ctrl.signal.aborted === true) {
+          return;
+        }
+        throw new Error(`Failed to send message. Server closed the connection unexpectedly.`);
+      },
+      onerror(err) {
+        throw err;
+      },
+      async onmessage(message) {
+        // console.log(message)
+        const event = message.event
+        const data = JSON.parse(message.data)
+
+        if (event === 'error') {
+          throw new Error(data.error);
+        }
+
+        if (event === 'done') {
+          if (currentConversation.value.id === null) {
+            currentConversation.value.id = data.conversationId
+            genTitle(currentConversation.value.id)
+          }
+          currentConversation.value.messages[currentConversation.value.messages.length - 1].id = data.messageId
+          abortFetch()
+          return;
+        }
+
+        messageQueue.push(data.content)
+        processMessageQueue()
+
+        scrollChatWindow()
+      },
+    })
