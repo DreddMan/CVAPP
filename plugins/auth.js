7919d41e868e15435f8d97e2699e1e38c141e9f8
DreddMan <109197519+DreddMan@users.noreply.github.com>
2022-06-18 22:22:51 +0000

    fix bug in last commit

--- plugins/auth.js
+++ plugins/auth.js
@@ -8,3 +8,47 @@ const AUTH_ROUTE = {
 const ENDPOINTS = {
     login: {
         url: '/api/account/login/'
+    },
+    user: {
+        url: '/api/account/user/'
+    }
+}
+
+
+export default defineNuxtPlugin(() => {
+
+    class Auth {
+        constructor() {
+            this.loginIn = useState('loginIn', () => false)
+            this.user = useState('user')
+        }
+
+        async logout () {
+            this.loginIn.value = false
+            this.user.value = null
+            await this.redirectToLogin()
+        }
+
+        setUser (user) {
+            this.user = user
+            this.loginIn.value = true
+        }
+
+        async fetchUser () {
+            const { data, error } = await useFetch(ENDPOINTS.user.url, {
+                // withCredentials: true
+            })
+            if (!error.value) {
+                this.setUser(data.value)
+                return null
+            }
+            return error
+        }
+
+        async redirectToLogin (callback) {
+            return await navigateTo(
+                AUTH_ROUTE.login + '?callback=' + encodeURIComponent(callback || AUTH_ROUTE.home)
+            )
+        }
+
+    }
