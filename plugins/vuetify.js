3069c12b132567b9c10ac69d6e6326b87121f623
DreddMan <109197519+DreddMan@users.noreply.github.com>
2022-06-27 16:54:01 +0000

    Update configs.

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,4 +1,5 @@
 import { createVuetify } from 'vuetify'
 import { aliases, md } from 'vuetify/iconsets/md'
 import * as components from 'vuetify/components'
-import { md3 } from 'vuet
+import { md3 } from 'vuetify/blueprints'
+// import * as directives fro
