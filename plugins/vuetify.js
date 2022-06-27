f337fe25d46425591468e5c900416c90efe24256
DreddMan <109197519+DreddMan@users.noreply.github.com>
2022-06-27 22:35:48 +0000

    Greek translation

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,4 +2,6 @@ import { createVuetify } from 'vuetify'
 import { aliases, md } from 'vuetify/iconsets/md'
 import * as components from 'vuetify/components'
 import { md3 } from 'vuetify/blueprints'
-// import * as directives fro
+// import * as directives from 'vuetify/directives'
+
+export default defineNu
